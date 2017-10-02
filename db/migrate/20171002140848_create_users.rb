618ac45578f3819eb49e7e11c66d43f6665242b6
github username <github email address>
2017-10-02 10:57:25 -0400

    user,reviews,restaurants

--- db/migrate/20171002140848_create_users.rb
+++ db/migrate/20171002140848_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.text :bio
+      t.string :bio
       t.integer :EXP
       t.string :favorite_food
 
