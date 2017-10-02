be6851671f8dc1b3b5925001750b20464a4ce4f8
github username <github email address>
2017-10-02 12:03:31 -0400

    sign up working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def welcome
+
+  end
+
+  def login
+
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:id] = @user.id
+       redirect_to '/home'
+     else
+       redirect_to '/user/new'
+     end
+  end
+
+  def home
+
+  end
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:bio,:EXP,:favorite_food,:password,:password_confirmation)
+  end
+
+
 end
