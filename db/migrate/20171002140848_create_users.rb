1be5d10fbcd6781f6f55bc8af4fbc8874c69cd30
github username <github email address>
2017-10-02 10:10:16 -0400

    commit test

---
+++ db/migrate/20171002140848_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.text :bio
+      t.integer :EXP
+
+
+      t.timestamps
+    end
+  end
+end
