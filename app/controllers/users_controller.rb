ad86de13b1e5708fe27753d6c33b10996c7cb254
github username <github email address>
2017-12-15 14:35:36 -0500

    demo auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in
+  # commented out for demo
+  # before_action :logged_in
   skip_before_action :logged_in, only: [:new, :create]
   def new
     @user = User.new
